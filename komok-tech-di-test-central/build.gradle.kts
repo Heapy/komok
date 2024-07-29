3b7ec00e9548a517448df5eab7ef017458e4e00f
Ruslan Ibragimov <ruslan@heapy.io>
2024-07-29 09:50:17 +0300

    Update komok-tech-di-test-central

--- komok-tech-di-test-central/build.gradle.kts
+++ komok-tech-di-test-central/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+
 plugins {
     kotlin("jvm").version("2.0.20-Beta2")
     id("com.google.devtools.ksp").version("2.0.20-Beta2-1.0.23")
@@ -8,8 +11,23 @@ repositories {
     mavenLocal()
 }
 
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks
+    .withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>()
+    .configureEach {
+        compilerOptions {
+            jvmTarget.set(JVM_21)
+            languageVersion.set(KotlinVersion.KOTLIN_2_0)
+            apiVersion.set(KotlinVersion.KOTLIN_2_0)
+            freeCompilerArgs.add("-Xcontext-receivers")
+        }
+    }
+
 dependencies {
     implementation(libs.logback)
-    ksp("io.heapy.komok:komok-tech-di:1.0.0")
-    implementation("io.heapy.komok:komok-tech-di-lib:1.0.0")
+    ksp("io.heapy.komok:komok-tech-di:1.0.1")
+    implementation("io.heapy.komok:komok-tech-di-lib:1.0.1")
 }
