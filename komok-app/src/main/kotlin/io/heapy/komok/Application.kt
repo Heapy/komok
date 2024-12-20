9eadc9749665ad94b250367d68fa315a290bc789
Ruslan Ibragimov <ruslan@heapy.io>
2024-12-20 18:47:26 +0300

    Convention: Module is a separate file

--- komok-app/src/main/kotlin/io/heapy/komok/Application.kt
+++ komok-app/src/main/kotlin/io/heapy/komok/Application.kt
@@ -1,6 +1,8 @@
 @file:JvmName("Application")
 package io.heapy.komok
 
+import io.heapy.komok.business.entrypoint.createKomokApplicationModule
+
 fun main() {
     createKomokApplicationModule {}
         .komokApplication
