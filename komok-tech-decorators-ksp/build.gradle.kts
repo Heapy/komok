985ea59c936ccd17e35f87ac4d802fb96ede239d
Ruslan Ibragimov <ruslan@heapy.io>
2025-04-18 23:10:37 +0300

    Add snapshot repository and new dependencies to build file
    Included the Sonatype snapshot repository for accessing snapshot artifacts. Updated dependencies to use the `kotlinPoet.ksp.snapshot` library and added a project dependency on `komokTechDecoratorsLib`.

--- komok-tech-decorators-ksp/build.gradle.kts
+++ komok-tech-decorators-ksp/build.gradle.kts
@@ -4,9 +4,11 @@ plugins {
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
 }
 
 dependencies {
-    implementation(libs.kotlin.poet)
+    implementation(libs.kotlin.poet.ksp.snapshot)
     implementation(libs.ksp)
+    implementation(projects.komokTechDecoratorsLib)
 }
